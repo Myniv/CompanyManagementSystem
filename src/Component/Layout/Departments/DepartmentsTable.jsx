d50c5f98528f6e519f65a32c4e4a0f04ce8eac5e
Mulyana N <mulyanan.ur26@gmail.com>
2024-11-07 08:56:56 +0700

    Add Departments Table

---
+++ src/Component/Layout/Departments/DepartmentsTable.jsx
@@ -0,0 +1,101 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const DepartmentsTable = () => {
+  const [departments, setDepartments] = useState([]);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedDepartments =
+      JSON.parse(localStorage.getItem("departments")) || [];
+    setDepartments(storedDepartments);
+  }, []);
+
+  const onDeleteDepartments = (deptNo) => {
+    const storedDepartments =
+      JSON.parse(localStorage.getItem("departments")) || [];
+    const deleteDepartments = storedDepartments.filter(
+      (b) => b.deptNo !== deptNo
+    );
+
+    localStorage.setItem("departments", JSON.stringify(deleteDepartments));
+
+    localStorage.setItem("departments", JSON.stringify(deleteDepartments));
+    setDepartments(deleteDepartments);
+  };
+
+  const onEditDepartments = (deptNo) => {
+    navigate(`/departments/${deptNo}`);
+  };
+
+  const onAddDepartments = () => {
+    navigate(`/departments/new`);
+  };
+
+  return (
+    <>
+      <div className="m-4">
+        <div className="d-flex justify-content-between align-items-center">
+          <h2>Departments Table</h2>
+        </div>
+        <table className="table table-striped table-bordered">
+          <thead className="thead-dark">
+            <tr>
+              <th scope="col">ID Department</th>
+              <th scope="col">Department Name</th>
+              <th scope="col">Department Manager</th>
+            </tr>
+          </thead>
+          <tbody>
+            {departments.map((departments) => (
+              <tr scope="row" key={departments.deptNo}>
+                <td>{departments.deptNo}</td>
+                <td>{departments.deptName}</td>
+                <td>{departments.mgrEmpNo}</td>
+                <td>
+                  <div className="d-grid gap-2 d-md-flex justify-content-md">
+                    <button
+                      type="button"
+                      className="btn btn-primary btn-sm"
+                      onClick={() => onEditDepartments(departments.empNo)}
+                      value={"edit"}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-danger btn-sm"
+                      onClick={() => onDeleteDepartments(departments.empNo)}
+                      value={"delete"}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+            <tr>
+              <td colSpan="9">
+                <div className="d-flex justify-content-end">
+                  <div className="d-grid gap-2 col-1">
+                    <button
+                      type="button"
+                      className="btn btn-primary btn-sm btn-block me-1"
+                      onClick={onAddDepartments}
+                    >
+                      Add Departments
+                    </button>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default DepartmentsTable

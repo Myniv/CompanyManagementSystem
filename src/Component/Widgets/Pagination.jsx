9dc205877ae072b681daf1b5f21849efad443e15
Mulyana N <mulyanan.ur26@gmail.com>
2024-11-14 18:43:19 +0700

    Update pagination to add icon previous and next

--- src/Component/Widgets/Pagination.jsx
+++ src/Component/Widgets/Pagination.jsx
@@ -5,13 +5,34 @@ const Pagination = ({
   handlePagination,
   currentPage,
 }) => {
+  const totalPages = Math.ceil(length / postsPerPage);
   let paginationNumber = [];
-  for (let i = 1; i <= Math.ceil(length / postsPerPage); i++) {
+  for (let i = 1; i <= totalPages; i++) {
     paginationNumber.push(i);
   }
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      handlePagination(currentPage - 1);
+    }
+  };
+
+  const goToNextPage = () => {
+    if (currentPage < totalPages) {
+      handlePagination(currentPage + 1);
+    }
+  };
   return (
     <div className="d-flex justify-content-center">
       <ul className="pagination">
+        <button
+          className="btn btn-outline-dark"
+          aria-label="Previous"
+          onClick={goToPreviousPage}
+          disabled={currentPage === 1}
+        >
+          <span aria-hidden="true">&laquo;</span>
+        </button>
         {paginationNumber.map((data) => (
           <li className="page-item" key={data}>
             <button
@@ -24,6 +45,14 @@ const Pagination = ({
             </button>
           </li>
         ))}
+        <button
+          className="btn btn-outline-dark"
+          aria-label="Next"
+          onClick={goToNextPage}
+          disabled={currentPage === totalPages}
+        >
+          <span aria-hidden="true">&raquo;</span>
+        </button>
       </ul>
     </div>
   );

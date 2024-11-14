b2f644db899d44a50574dacb1f2152fdf772a69a
Mulyana N <mulyanan.ur26@gmail.com>
2024-11-14 18:55:03 +0700

    Add card to the mainpage

---
+++ src/Component/Elements/Card.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+const Card = ({ title, description }) => {
+  return (
+    <div className="card text-center m-3" style={{ width: "18rem" }}>
+      <div className="card-body">
+        <h5 className="card-title bg-dark text-white p-2 rounded">{title}</h5>
+        <p
+          className="card-text"
+          style={{ fontSize: "2rem", fontWeight: "bold" }}
+        >
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/Page/MainPage.jsx
+++ src/Page/MainPage.jsx
@@ -1,7 +1,35 @@
+import { useDispatch, useSelector } from "react-redux";
+import { fetchDepartment } from "../redux/Slicer/departmentSlicer";
+import { fetchEmployee } from "../redux/Slicer/employeeSlicer";
+import { fetchProject } from "../redux/Slicer/projectSlicer";
+import { fetchAssignment } from "../redux/Slicer/assignmentSlicer";
+import { useEffect } from "react";
+import Card from "../Component/Elements/Card";
+
 const MainPage = () => {
+  const dispatch = useDispatch();
+  const department = useSelector((state) => state.department);
+  const employee = useSelector((state) => state.employee);
+  const project = useSelector((state) => state.project);
+  const assignment = useSelector((state) => state.assignment);
+
+  useEffect(() => {
+    dispatch(fetchDepartment());
+    dispatch(fetchEmployee());
+    dispatch(fetchProject());
+    dispatch(fetchAssignment());
+  }, []);
   return (
     <>
-      <h2>Welcome to the Company Management</h2>
+      <div className="container text-center my-4">
+        <h2>Welcome to the Company Management</h2>
+        <div className="container d-flex justify-content-center">
+          <Card title={"Department"} description={department.data.length} />
+          <Card title={"Employee"} description={employee.data.length} />
+          <Card title={"Project"} description={project.data.length} />
+          <Card title={"Assignment"} description={assignment.data.length} />
+        </div>
+      </div>
     </>
   );
 };

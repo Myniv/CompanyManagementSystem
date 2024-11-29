1fc0b0d835d2065d4570fddfe8a1024373197f7e
Mulyana N <mulyanan.ur26@gmail.com>
2024-11-29 16:43:42 +0700

    Updata baseapi

--- src/baseApi.jsx
+++ src/baseApi.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
-import { refreshToken } from "./redux/Slicer/authSlicer";
+import { refreshToken, logout } from "./redux/Slicer/authSlicer";
+import { store } from "./redux/store";
 
 const baseApi = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
@@ -13,12 +14,14 @@ baseApi.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
+    if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
       try {
         await refreshToken();
+        await store.dispatch(refreshToken()).unwrap();
         return baseApi(originalRequest);
       } catch (refreshError) {
+        await store.dispatch(logout());
         return Promise.reject(refreshError);
       }
     }

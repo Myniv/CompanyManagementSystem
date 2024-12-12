d7e87f4d3e64041603bdda6b3e0e9c3a1b4456f5
Mulyana N <mulyanan.ur26@gmail.com>
2024-12-12 20:18:16 +0700

    Update reportsPage input id using select type not input number

--- src/Page/ReportsPage.jsx
+++ src/Page/ReportsPage.jsx
@@ -2,13 +2,20 @@ import ReportsWithDate from "../Component/Widgets/ReportsWithDate";
 import { Tab, Tabs } from "react-bootstrap";
 import ReportsWithoutDate from "../Component/Widgets/ReportsWithoutDate";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchDepartment } from "../redux/Slicer/departmentSlicer";
 
 const ReportsPage = () => {
   const [id, setId] = useState("");
   //   const [hidden, setHidden] = useState(true);
 
   const { user: currentUser } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+  const department = useSelector((state) => state.department);
+  useEffect(() => {
+    dispatch(fetchDepartment());
+  }, []);
 
   useEffect(() => {
     if (currentUser.roles.includes("Department Manager")) {
@@ -27,47 +34,59 @@ const ReportsPage = () => {
   return (
     <>
       <div className="container mt-4">
-        <Tabs defaultActiveKey="leaveReport" id="report-tabs" className="mb-3">
-          {currentUser.roles.includes("HR Manager") ||
-            (currentUser.roles.includes("Administrator") && (
-              <Tab eventKey="leaveReport" title="Leave Report">
-                <h4>Leave Report</h4>
-                <ReportsWithDate apiUrl="http://localhost:5045/api/v1/Company/leave-report-pdf" />
-              </Tab>
-            ))}
-
-          {currentUser.roles.includes("Department Manager") ||
-            currentUser.roles.includes("HR Manager") ||
-            (currentUser.roles.includes("Administrator") && (
-              <Tab eventKey="employeeReport" title="Employee Report">
-                <h4>Employee Report</h4>
-                <div className="col-md-4 mb-3">
-                  <label className="form-label">Id</label>
-                  <input
-                    type="number"
-                    className="form-control"
-                    value={id}
-                    disabled={
-                      currentUser.roles.includes("Department Manager")
-                        ? true
-                        : false
-                    }
-                    onChange={(e) => setId(e.target.value)}
-                  />
-                </div>
-
-                {id && (
-                  <ReportsWithoutDate
-                    apiUrl={`http://localhost:5045/api/v1/Employees/report-pdf/${id}`}
-                  />
-                )}
-              </Tab>
-            ))}
-
+        <Tabs
+          defaultActiveKey="projectReport"
+          id="report-tabs"
+          className="mb-3"
+        >
           <Tab eventKey="projectReport" title="Project Report">
             <h4>Project Report</h4>
             <ReportsWithoutDate apiUrl="http://localhost:5045/api/v1/Projects/report-pdf" />
           </Tab>
+
+          {(currentUser.roles.includes("HR Manager") ||
+            currentUser.roles.includes("Administrator")) && (
+            <Tab eventKey="leaveReport" title="Leave Report">
+              <h4>Leave Report</h4>
+              <ReportsWithDate apiUrl="http://localhost:5045/api/v1/Company/leave-report-pdf" />
+            </Tab>
+          )}
+
+          {(currentUser.roles.includes("Department Manager") ||
+            currentUser.roles.includes("HR Manager") ||
+            currentUser.roles.includes("Administrator")) && (
+            <Tab eventKey="employeeReport" title="Employee Report">
+              <h4>Employee Report</h4>
+              <div className="col-md-4 mb-3">
+                <label htmlFor="deptno" className="form-label">
+                  Select Department
+                </label>
+                <select
+                  id="deptno"
+                  name="deptno"
+                  className={`form-select`}
+                  value={id}
+                  onChange={(e) => setId(e.target.value)}
+                  required
+                >
+                  <option value="" disabled selected>
+                    Select Department
+                  </option>
+                  {department.data.map((department) => (
+                    <option key={department.deptno} value={department.deptno}>
+                      {department.deptname}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              {id && (
+                <ReportsWithoutDate
+                  apiUrl={`http://localhost:5045/api/v1/Employees/report-pdf/${id}`}
+                />
+              )}
+            </Tab>
+          )}
         </Tabs>
       </div>
     </>
